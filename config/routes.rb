fbe8ee04a8c256d35be8d7045ec8366af678ea4c
Steven Wanderski <steven.wanderski@gmail.com>
2022-10-13 10:10:45 -0500

    Adds the first example

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/install', to: 'pages#install'
   get '/options', to: 'pages#options'
   get '/examples', to: 'pages#examples'
+
+  get '/examples/image-slideshow-captions', to: 'pages/examples#image_slideshow_captions'
 end
