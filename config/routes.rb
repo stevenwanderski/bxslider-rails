a597e3d271013be4c27676f991881d02ed432f39
Steven Wanderski <steven.wanderski@gmail.com>
2022-10-21 12:19:16 -0500

    Adds two more examples

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   get '/examples/image-slideshow-captions', to: 'pages/examples#image_slideshow_captions'
   get '/examples/auto-show-start-stop-controls', to: 'pages/examples#auto_show_start_stop_controls'
+  get '/examples/manual-show-without-infinite-loop', to: 'pages/examples#manual_show_without_infinite_loop'
+  get '/examples/slideshow-adaptive-height', to: 'pages/examples#slideshow_adaptive_height'
 end
